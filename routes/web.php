2be9816468c9070b3816c1f0624ab2f1e24d7986
Hagar Elbakry <hagarelbakry12@gmail.com>
2025-08-08 22:41:33 +0300

    add routes for home, resume, projects and contact pages

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\ResumeController;
 use Illuminate\Support\Facades\Route;
+use function Pest\Laravel\get;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/resume', [ResumeController::class, 'index'])->name('resume');
+Route::get('/projects', [ProjectController::class, 'index'])->name('projects');
+Route:get('/contact', [ContactController::class, 'index'])->name('contact');
+Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
